6085a839fc7a71ac3137ea37b8eb41b47d686bfd
Alex <alexurban12@gmail.com>
2021-11-24 09:44:30 +0200

    add metamask connect button, TODO: functionality

---
+++ frontend/src/lib/components/WalletBtn.svelte
@@ -0,0 +1,47 @@
+<script lang="ts" context="module">
+	declare global {
+		interface Window { ethereum: any}
+	}
+</script>
+
+<script lang="ts">
+	import { isConnected } from '../stores';
+
+	
+
+	function connect() {
+		if ($isConnected) {
+			// do nothing if already connected
+			alert("Already connected") // TODO remove
+			return;
+		}
+
+		if (typeof window.ethereum !== 'undefined') {
+			alert('MetaMask is installed!');
+
+			$isConnected = true;
+		} else {
+			alert("Please install metamask")
+		}
+	}
+</script>
+
+<div id="connect" on:click={connect}>Connect Wallet</div>
+
+<style lang="scss">
+	$btn-color: grey; 
+
+	#connect {
+		background: $btn-color;
+
+		padding: 30px;
+		margin: 0 50px;
+		border-radius: 10px;
+
+		&:hover {
+			background: lighten($btn-color, 10%);
+			cursor: pointer;
+			
+		}
+	}
+</style>
---
+++ frontend/src/routes/__layout.svelte
@@ -0,0 +1,45 @@
+<script lang='ts'>
+    import WalletBtn from "$lib/components/WalletBtn.svelte";
+</script>
+
+
+<svelte:head>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito&display=swap">
+    <script src="https://kit.fontawesome.com/30f595e84f.js" crossorigin="anonymous"></script>
+</svelte:head>
+
+<header>
+    <div class="header-wrapper">
+        <WalletBtn/>
+    </div>
+</header>
+
+<slot />
+
+<style lang="scss">
+	:global(body) {
+		margin: 0;
+        padding: 0;
+		/* font: 16px/1.6em -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji"; */
+		font-family: 'Nunito';
+	}
+
+	:global(*) {
+		box-sizing: border-box;
+	}
+
+	header {
+		height: 200px;
+        width: 100%;
+        background: black;
+
+        .header-wrapper {
+            height: 100%;
+            width: 100%;
+
+            display: flex;
+            align-items: center;
+            justify-content: flex-end;
+        }
+	}
+</style>
