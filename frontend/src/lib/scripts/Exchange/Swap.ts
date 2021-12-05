e00d7d279c5d81550b2fe1d8cd1fa36d6c6d4554
Alex <alexurban12@gmail.com>
2021-12-05 17:57:43 +0200

    get balance working

--- frontend/src/lib/scripts/Exchange/Swap.ts
+++ frontend/src/lib/scripts/Exchange/Swap.ts
@@ -2,7 +2,7 @@ import type { Bytes32, Uint256, Uint32, Address } from 'soltypes';
 import { isProvided, provider, router, signer } from '$lib/stores';
 import { ethers } from 'ethers'
 import {ERC20} from '$lib/abis/ERC20';
-import type { Ierc20 } from '$lib/typesUsed/Ierc20'
+import type { MyToken } from '$lib/typesUsed/MyToken'
 import type { Web3Provider, JsonRpcSigner } from '@ethersproject/providers';
 
 
@@ -23,10 +23,17 @@ provider.subscribe(provider => {
  */
 export async function getBalance(address: string) {
     if (!provider_val || !signer_val) throw new Error("Provider or signer do not exist yet");
-    const erc20Contract = new ethers.Contract(address, ERC20, provider_val) as Ierc20
-    const bal = await erc20Contract.balanceOf(await signer_val.getAddress())
-    console.log(bal)
-    return 200;
+    const erc20Contract = new ethers.Contract(address, ERC20, provider_val) as MyToken
+    const bal = await getErc20Balance(erc20Contract, await getSignerAddress(signer_val));
+    return bal;
+}
+
+async function getErc20Balance(contract: MyToken, address: string) {
+    return (await contract.balanceOf(address)).div(ethers.constants.WeiPerEther).toString();
+}
+
+async function getSignerAddress(signer: JsonRpcSigner) {
+    return await signer_val.getAddress()
 }
 
 export async function getOtherNumTokens(
